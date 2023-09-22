78f41efd4f190b5bb53ec6bc4793ff00a8201977
jucheolkang <jucheol9877@naver.com>
2023-09-22 22:40:32 +0900

    add MenuRepository

---
+++ src/main/java/com/project/springapistudy/entity/MenuRepository.java
@@ -0,0 +1,10 @@
+package com.project.springapistudy.entity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface MenuRepository extends JpaRepository<MenuEntity,Integer> {
+
+}
